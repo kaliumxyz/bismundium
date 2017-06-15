259864a01c024d08a7f6b04d0945ecaee6c35731
kaliumxyz <martijn@becker.wtf>
2017-06-15 21:46:12 +0200

    added basic communications.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict'
+// require socket.io and set it up to listen to port 31337.
 const io = require('socket.io')(31337)
 
-io.on('client-input', console.log)
+// todo:
+// - Add a function to read out the DB.
+
+
+io.on('connection', socket => {
+	socket.emit('connected', 'msgs')
+	socket.on('client-input', console.log)
+})
