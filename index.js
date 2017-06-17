54858f92a1d2f9975b68145017240a3c0d20f9e2
kaliumxyz <martijn@becker.wtf>
2017-06-17 14:27:58 +0200

    proof of concept done.

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 'use strict'
 const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database('./xkcd')
+const euphoriaConnection = require('euphoria-connection')
+const connection = new euphoriaConnection('xkcd')
 // require ws and set it up to listen to port 31337.
 const WebSocket = require('ws')
 const ws = new WebSocket.Server({ port: 31337 })
 
 const fif = require('find-in-files')
+connection.once('open', _ => {
+	db.serialize(_ => {
+		db.run('DROP TABLE username')
+		db.run("CREATE TABLE username (info TEXT)")
+		const stmt = db.prepare("INSERT INTO username VALUES (?)")
+		function handleReply(data) {
+			console.log(data)
+			data.data.log.forEach((x, i) => stmt.run(`${x.sender.name} ${i}`))
+			stmt.finalize()
+			db.each("SELECT rowid AS id, info FROM username", function (err, row) {
+				console.log(row.id + ": " + row.info)
+			})
+		}
+		connection.download(10, null, handleReply)
+	})
 
-db.serialize( _ => {
-	db.run('DROP TABLE username')
-	db.run("CREATE TABLE username (info TEXT)")
-	  var stmt = db.prepare("INSERT INTO username VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM username", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
 })
+
 
 
 
@@ -29,24 +35,19 @@ db.serialize( _ => {
 
 ws.on('connection', socket => {
 
-	console.log("User connected", socket)
+	console.log("User connected")
 	socket.send('connected')
-	socket.on('message', 
+	socket.on('message',
 		data => {
 			socket.send(data, console.error)
 			console.log(data)
-			fif.find(data, './database/', '.log$').then( 
-			//data => data.forEach(x => console.log(x))
-			data => {
-				for(let results in data){
-				console.log(results)
-				console.log(data[results])
-				socket.send(JSON.stringify(data[results]), console.error).bind(socket)
-			}}
-		, console.error, console.log)
-		}
-	)
-	socket.on("disconnect", ev => {
-		console.log('disconnected:' + socket)
-	})
+			db.serialize(_ => {
+				db.each("SELECT info FROM username", function (err, row) {
+					socket.send(row.info, console.error)
+				})
+			})
+			socket.on("disconnect", ev => {
+				console.log('disconnected:' + socket)
+			})
+})
 })
