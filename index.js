2278708dedfcc313b5bfa7c07114bd9cd6da66e2
kalium <martijn@becker.wtf>
2017-10-04 04:18:11 +0200

    [GUI] rendering rework

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-'use strict'
-const WebSocket = require('ws')
-// pg = PostgreSQL
-const { Client } = require('pg')
-require('dotenv').config()
-const ws = new WebSocket.Server({ port: process.env.port })
-
-
-async function connect () {
-
-	const client = new Client(`postgres://${process.env.db_user}:${process.env.db_pass}@localhost:5432/euphoria`)
-
-	await client.connect()
-	return client
-}
-
-connect()
-	.catch(console.error)
-	.then( con => {
-		console.log('connected')
-
-	})
-
-// db.open('./xkcd').then( x => x.get("INSERT INTO XKCD VALUES PEW"), _ => process.abort())
-// todo:
-// - Add a function to read out the DB.
-
-// ws.on('connection', socket => {
-// 	console.log("User connected")
-// 	socket.send('connected')
-// 	socket.on('message',
-// 		data => {
-// 			socket.send(data, console.error)
-// 			console.log(data)
-// 			db.serialize(_ => {
-// 				db.each(`SELECT * FROM xkcd WHERE content LIKE '${data}'`, function (err, row) {
-// 					socket.send(JSON.stringify(row))
-// 				})
-// 			})
-// 		})
-// 	socket.on("disconnect", ev => {
-// 		console.log('disconnected:' + socket)
+// 'use strict'
+// const WebSocket = require('ws')
+// // pg = PostgreSQL
+// const { Client } = require('pg')
+// require('dotenv').config()
+// const ws = new WebSocket.Server({ port: process.env.port })
+//
+//
+// async function connect () {
+//
+// 	const client = new Client(`postgres://${process.env.db_user}:${process.env.db_pass}@localhost:5432/euphoria`)
+//
+// 	await client.connect()
+// 	return client
+// }
+//
+// connect()
+// 	.catch(console.error)
+// 	.then( con => {
+// 		console.log('connected')
+//
 // 	})
-// })
+//
+// // db.open('./xkcd').then( x => x.get("INSERT INTO XKCD VALUES PEW"), _ => process.abort())
+// // todo:
+// // - Add a function to read out the DB.
+//
+// // ws.on('connection', socket => {
+// // 	console.log("User connected")
+// // 	socket.send('connected')
+// // 	socket.on('message',
+// // 		data => {
+// // 			socket.send(data, console.error)
+// // 			console.log(data)
+// // 			db.serialize(_ => {
+// // 				db.each(`SELECT * FROM xkcd WHERE content LIKE '${data}'`, function (err, row) {
+// // 					socket.send(JSON.stringify(row))
+// // 				})
+// // 			})
+// // 		})
+// // 	socket.on("disconnect", ev => {
+// // 		console.log('disconnected:' + socket)
+// // 	})
+// // })
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,39 +1,118 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', _ => {
-	const socket = new WebSocket('ws://localhost')
-	const input = document.querySelector('input')
-	const list = hyperHTML.bind(document.getElementById('list'))
-	const alarm = hyperHTML.bind(document.getElementById('alert'))
-	socket.onopen = _ => input.disabled = false
-	let timeout
-	// sends a search request to the server.
-	function search() {
-		socket.send(JSON.stringify({ "type": "search", "data": input.value }))
+window.addEventListener('load',_ => {
+	'use strict'
+	function toggleBackground(){
+		hyperHTML.bind(document.querySelector('.background'))` `.remove()
 	}
-	// formats the incoming data into an unordered list.
-	function format() {
+	hyperHTML.bind(document.body)`
+	<div class='container-fluid' id="main">
+		<div class='row align-items-center content'>
+			<div class='col-1 col-md-4'></div>
 
-	}
-	socket.onopen = data => {
-		alarm`
+			<div class='col-10 col-md-4 text-center card'>
+				<div class="card-body">
+					<h4 class="card-title">Search <span class='blue'>&amp;</span><span oninput='this.classList.add("err")' class='blue room' contenteditable=true>xkcd</span> history</h4>
+						<div class="alert alert-info" role="alert" id='alert'>
+							connecting...
+						</div>
+					<div class='card-block'>
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Search for...">
+							<span class="input-group-btn">
+								<button class="btn btn-secondary" type="button">Go!</button>
+							</span>
+						</div>
+					</div>
+				</div>
+				<div class='card-block'>
+					<ul class='list-group mock' id='list'>
+						<li class='list-group-item'> - </li>
+						<li class='list-group-item'> - </li>
+						<li class='list-group-item'> - </li>
+						<li class='list-group-item'> - </li>
+						<li class='list-group-item'> - </li>
+						<li class='list-group-item'> - </li>
+					</ul>
+				</div>
+				<div class="card-body">
+				</div>
+			</div>
+			<div class='col-1 col-md-4'></div>
+		</div>
+	</div>
+	<header class="header ">
+		<div class="container-fluid">
+			<div class="row">
+			<span class="col-4">
+			</span>
+				<span class="text-muted col-4"></span>
+				<span class="settings col-4 text-right">
+					<!-- add a quick way to toggle -->
+				<a onclick=${toggleBackground}> <i class="fa fa-gears fa-lg"></i></a>
+			</span>
+			</div>
+		</div>
+	</header>
+	<footer class="footer ">
+		<div class="container-fluid">
+			<div class="row">
+			<span class="col-4">
+			<a href="https://github.com/kaliumxyz/bismundium/"> <i class="fa fa-github fa-lg"></i></a>
+			</span>
+			<span class="text-muted col-4"></span>
+			<span class="settings col-4 text-right">
+			</span>
+			</div>
+		</div>
+	</footer>
+
+	<div id="particles-js" class="background"></div>
+		`
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	particlesJS.load('particles-js', 'assets/particles.json', _ => {
+		const socket = new WebSocket('ws://localhost')
+		const input = document.querySelector('input')
+		const list = hyperHTML.bind(document.getElementById('list'))
+		const alarm = hyperHTML.bind(document.getElementById('alert'))
+		socket.onopen = _ => input.disabled = false
+		let timeout
+		// sends a search request to the server.
+		function search() {
+			socket.send(JSON.stringify({ "type": "search", "data": input.value }))
+		}
+		// formats the incoming data into an unordered list.
+		function format() {
+
+		}
+		socket.onopen = data => {
+			alarm`
 		Connected.
 		`.classList.replace('alert-info', 'alert-success')
-		
-	
-		const timeout = window.setTimeout(_ => {alarm``.hidden = true}, 1000)
-		
-	}
-	socket.onerror = data => {
-		alarm`
+
+
+			const timeout = window.setTimeout(_ => {alarm``.hidden = true}, 1000)
+
+		}
+		socket.onerror = data => {
+			alarm`
 		Our backend is currently offline.
 		`.classList.replace('alert-info', 'alert-danger')
-	}
-	socket.onmessage = data => {
-		list`<li class='list-group-item'> ${JSON.stringify(data.data)} </li>`
-	}
-	input.onkeypress = ev => {
-		window.clearTimeout(timeout)
-		timeout = window.setTimeout(suggest, 600)
-	}
+		}
+		socket.onmessage = data => {
+			list`<li class='list-group-item'> ${JSON.stringify(data.data)} </li>`
+		}
+		input.onkeypress = ev => {
+			window.clearTimeout(timeout)
+			timeout = window.setTimeout(suggest, 600)
+		}
 
+	})
 })
+
+function match(el){
+	rooms.find(x => x === el.innerText)
+}
+
+function toggleBackground(){
+	'use strict'
+	hyperHTML.bind(document.querySelector('.background'))` `
+}
